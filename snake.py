c5a968abb9a1d4aa116b07b43820c106c168a2b8
Exzerin <exzerin@gmail.com>
2021-05-21 23:40:13 -0400

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,10 +1,8 @@
-# wip
-
 import curses
 
 # setup windows
 curses.initscr()
-win + curses.newin(20, 60, 0, 0) # y,x
+win = curses.newwin(20, 60, 0, 0) # y,x
 win.keypad(1)
 curses.noecho()
 curses.curs_set(0)
@@ -13,7 +11,7 @@ win.nodelay(1) # -1
 
 # snake and food
 snake = [(4, 10), (4, 9), (4, 8)]
-food = ()
+food = (10, 20)
 
 # game logic
 
@@ -22,6 +20,10 @@ score = 0
 while True:
         event = win.getch()
         # ...
+        for C in snake:
+            win.addch(c[0], c[1], '*')
+            
+        win.addch(food[0], food[1], '#')    
         
 curses.endwin()
 print(f"Final score= {score}")
